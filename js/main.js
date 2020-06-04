1541c567e3f0aa425cab23dcc2980551cac64e35
Alexander Lucas <kouva.nero@gmail.com>
2020-06-03 21:13:12 -0500

    added button needs work

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 'use strict'
 
 console.log('this file is working');
+
+funtion(myFunction)
+    var txt;
+    var person = prompt("Enter your email for contact back:, 'email@deltastore.com');
+     if (person == null || person == "")
+        txt = "User cancelled input";
+    else
+        text = "Thank you " + person + " we'll get in touch soon!";
+    document.getElementById('demo').innerHTML = txt;
