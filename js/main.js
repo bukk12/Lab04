83dc266000bb97623115b93375c66a34939b839f
Alexander Lucas <kouva.nero@gmail.com>
2020-06-04 10:25:56 -0500

    finished JS

--- js/main.js
+++ js/main.js
@@ -2,11 +2,36 @@
 
 console.log('this file is working');
 
-funtion(myFunction)
-    var txt;
-    var person = prompt("Enter your email for contact back:, 'email@deltastore.com');
-     if (person == null || person == "")
-        txt = "User cancelled input";
-    else
-        text = "Thank you " + person + " we'll get in touch soon!";
-    document.getElementById('demo').innerHTML = txt;
+// funtion(myFunction)
+//     var txt;
+//     var person = prompt("Enter your email for contact back:, 'email@deltastore.com');
+//      if (person == null || person == "")
+//         txt = "User cancelled input";
+//     else
+//         text = "Thank you " + person + " we'll get in touch soon!";
+//     document.getElementById('demo').innerHTML = txt;
+
+var button = document.createElement("button"); {
+button.innerHTML = "*Click me to leave your email*";
+
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+}
+
+
+
+button.addEventListener ("click", function() {
+   var eMail = prompt("Please enter your email and date visited:");
+  });
+
+  var fs = require('fs');
+  var path = "../output.txt";
+  var data = "eMail";
+  fs.appendFile(path, data, function(error) {
+    if (error){
+         console.error("write error: " + error.message);
+    } else {
+         console.log("successful Write to " + path);
+    }
+ });
